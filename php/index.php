a5bfab89149f03e2c15b9ba5f6344e3f5726355b
Kauã <47856908+kauarijoaguiar@users.noreply.github.com>
2021-09-26 15:01:58 -0300

    NÃO AGUENTO MAIS

--- php/index.php
+++ php/index.php
@@ -13,7 +13,7 @@ $invalidos = 0;
 if (!isset($_POST["cpf"])) {
 	$invalidos++;
 } else {
-	if (!preg_match("#^[0-9]{11}$#", $_POST["cpf"])) {
+	if (!preg_match("#^\d{3}\.?\d{3}\.?\d{3}-?\d{2}$#", $_POST["cpf"])) {
 		$invalidos++;
 	}
 }
@@ -23,6 +23,41 @@ if ($invalidos == 0) {
 } else {
 	echo "com erros";
 }
+
+//validar CPF
+/*
+function validar(){
+	$cpf = preg_replace('/[^0-9]/', "", $number);
+	
+	//quantidade de numeros ou se tem algum numero repetido 
+	if (strlen($cpf) != 11 || preg_match('/([0-9])\1{10}/', $cpf) {
+        return false;
+    }
+
+$number_quantity_to_loop = [9, 10];
+
+    foreach ($number_quantity_to_loop as $item) {
+
+        $sum = 0;
+        $number_to_multiplicate = $item + 1;
+    
+        for ($index = 0; $index < $item; $index++) {
+
+            $sum += $cpf[$index] * ($number_to_multiplicate--);
+    
+        }
+
+        $result = (($sum * 10) % 11);
+
+        if ($cpf[$item] != $result) {
+            return false;
+        }
+
+    }
+	return true;
+}
+}
+*/
 
 ?>
 </body>
