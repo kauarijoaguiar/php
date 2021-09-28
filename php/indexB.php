984fe3edbaa64c412a33c56df02076a74ab4e0ec
Kauã <47856908+kauarijoaguiar@users.noreply.github.com>
2021-09-28 20:25:44 -0300

    A

--- php/indexB.php
+++ php/indexB.php
@@ -8,27 +8,8 @@ var_dump($_POST);
 echo "</pre>";
 echo "<hr>";
 
-$invalidos = 0;
-if (!isset($_POST["dias"])) {
-	$invalidos++;
-} else {
-	if (!preg_match("#^(0|[1-9][0-9]*)$#", $_POST["dias"])) {
-		$invalidos++;
-	}
-}
-if (!isset($_POST["data"])) {
-	$invalidos++;
-} else {
-	if (!preg_match("#^(0?[1-9]|[12][0-9]|3[01])/(0?[1-9]|1[0-2])/(19|2[0-9])[0-9]{2}$#", $_POST["data"])) {
-		$invalidos++;
-	}
-}
-
-if ($invalidos == 0) {
-	echo "sem erros";
-	// código PHP realizando alguma tarefa com os dados recebidos
-} else {
-	echo "com erros";
+function maisdias(){
+	
 }
 
 ?>
