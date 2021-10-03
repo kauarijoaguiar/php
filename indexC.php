653f8e7bbc9f04a5680194cfbb3b430d07df64ca
juliapcp <juliapcp2003@gmail.com>
2021-10-03 00:50:58 -0300

    3

--- indexC.php
+++ indexC.php
@@ -9,10 +9,10 @@ echo "</pre>";
 echo "<hr>";
 
 if (verificar($valor) == true) {
-	//nome
 	echo "valor esta dentro dos parametros";
    echo "<br>";
 	 echo $_POST["valor"];
+	 echo $_POST[valorMonetarioPorExtenso($valor)];
  } else {
 	 echo "valor n esta dentro dos parametros";
  }
@@ -26,6 +26,60 @@ function verificar($valor){
 		return false;
 	}
 }
+
+ function valorMonetarioPorExtenso( $valor = 0 )
+    {
+ 
+        $singular = array("centavo", "real", "mil", "milhão");
+        $plural = array("centavos", "reais", "mil", "milhões");
+       
+        $c = array("", "cem", "duzentos", "trezentos", "quatrocentos","quinhentos", "seiscentos", "setecentos", "oitocentos", "novecentos");
+        $d = array("", "dez", "vinte", "trinta", "quarenta", "cinquenta","sessenta", "setenta", "oitenta", "noventa");
+        $d10 = array("dez", "onze", "doze", "treze", "quatorze", "quinze","dezesseis", "dezessete", "dezoito", "dezenove");
+        $u = array("", "um", "dois", "três", "quatro", "cinco", "seis","sete", "oito", "nove");
+        $z = 0;
+ 
+        $valor = number_format( $valor, 2, ".", "." );
+        $inteiro = explode( ".", $valor );
+ 
+        for ( $i = 0; $i < count( $inteiro ); $i++ ) 
+        {
+            for ( $ii = mb_strlen( $inteiro[$i] ); $ii < 3; $ii++ ) 
+            {
+                $inteiro[$i] = "0" . $inteiro[$i];
+            }
+        }
+ 
+        $rt = null;
+        $fim = count( $inteiro ) - ($inteiro[count( $inteiro ) - 1] > 0 ? 1 : 2);
+        for ( $i = 0; $i < count( $inteiro ); $i++ )
+        {
+            $valor = $inteiro[$i];
+            $rc = (($valor > 100) && ($valor < 200)) ? "cento" : $c[$valor[0]];
+            $rd = ($valor[1] < 2) ? "" : $d[$valor[1]];
+            $ru = ($valor > 0) ? (($valor[1] == 1) ? $d10[$valor[2]] : $u[$valor[2]]) : "";
+ 
+            $r = $rc . (($rc && ($rd || $ru)) ? " e " : "") . $rd . (($rd && $ru) ? " e " : "") . $ru;
+            $t = count( $inteiro ) - 1 - $i;
+            $r .= $r ? " " . ($valor > 1 ? $plural[$t] : $singular[$t]) : "";
+            if ( $valor == "000")
+                $z++;
+            elseif ( $z > 0 )
+                $z--;
+                
+            if ( ($t == 1) && ($z > 0) && ($inteiro[0] > 0) )
+                $r .= ( ($z > 1) ? " de " : "") . $plural[$t];
+                
+            if ( $r ) {
+                $rt = $rt . ((($i > 0) && ($i <= $fim) && ($inteiro[0] > 0) && ($z < 1)) ? ( ($i < $fim) ? ", " : " e ") : " ") . $r;
+			}
+        }
+ 
+        $rt = mb_substr( $rt, 1 );
+ 
+        return($rt ? trim( $rt ) : "zero");
+ 
+    }
 ?>
 </body>
 </html>
