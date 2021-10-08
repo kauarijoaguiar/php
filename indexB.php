df1d79e84ed693df7c25b4fd1ed468c9f3019531
Kauã <47856908+kauarijoaguiar@users.noreply.github.com>
2021-10-08 17:07:05 -0300

    passando mal

--- indexB.php
+++ indexB.php
@@ -15,38 +15,18 @@ function mais($data, $dias){
     $data = implode('-', array_reverse(explode('/', $_POST['data'])));
 
 $contador = 0;
+$contador2 = 0;
+$somadeDU = date('d/m/Y', strtotime("+{$dias} days", strtotime($data)));
 
 while ($contador < $dias){
+    if(date("w",$data == 0)) {
+         $soma1 = $dias + 1;
+    }if(date("w",$data == 6)){
+        $soma1 = $dias + 2;
 
-    if((date("W",$data == 0)) || (date("W",$data == 6))) {
-         $soma = $dias + 1;
     }
-    if((date("W",$data != 0)) && (date("W",$data != 6))) {
-        $somadeDU = date('d/m/Y', strtotime("+{$soma} days", strtotime($data)));
-        $contador++;
-   }
-
+$contador++;
 }
-
-/*
-$feriados = [
-    
-    mktime(0, 0, 0, 1, 1, $ano),   
-    mktime(0, 0, 0, 4, 21, $ano),  
-    mktime(0, 0, 0, 5, 1, $ano),   
-    mktime(0, 0, 0, 9, 7, $ano),   
-    mktime(0, 0, 0, 10, 12, $ano), 
-    mktime(0, 0, 0, 11, 2, $ano), 
-    mktime(0, 0, 0, 11, 15, $ano), 
-    mktime(0, 0, 0, 12, 25, $ano), 
-    mktime(0, 0, 0, $mesPacoa, $diaPascoa - 48, $anoPascoa),
-    mktime(0, 0, 0, $mesPacoa, $diaPascoa - 47, $anoPascoa), 
-    mktime(0, 0, 0, $mesPacoa, $diaPascoa - 2, $anoPascoa),  
-    mktime(0, 0, 0, $mesPacoa, $diaPascoa, $anoPascoa),   
-    mktime(0, 0, 0, $mesPacoa, $diaPascoa + 60, $anoPascoa), 
-
-];
-*/
     $somadeDU = date('d/m/Y', strtotime("+{$soma} days", strtotime($data)));
 return $somadeDU;
 }
