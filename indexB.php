7c93290c030623894904dd1cfe0e669fc8e55ec3
juliapcp <juliapcp2003@gmail.com>
2021-10-09 00:53:44 -0300

    2

--- indexB.php
+++ indexB.php
@@ -14,21 +14,24 @@ function mais($data, $dias){
 
     $data = implode('-', array_reverse(explode('/', $_POST['data'])));
 
+
 $contador = 0;
 $contador2 = 0;
 $somadeDU = date('d/m/Y', strtotime("+{$dias} days", strtotime($data)));
 
 while ($contador < $dias){
     if(date("w",$data == 0)) {
-         $soma1 = $dias + 1;
-    }if(date("w",$data == 6)){
+        
+        $soma1 = $dias + 1;
+    } if(date("w",$data == 6)){
         $soma1 = $dias + 2;
-
+        
     }
-$contador++;
-}
-    $somadeDU = date('d/m/Y', strtotime("+{$soma} days", strtotime($data)));
-return $somadeDU;
+    $contador++;
+}  
+
+    $somadeDU = date('d/m/Y', strtotime("+{$soma1} days", strtotime($data)));
+    return $somadeDU;
 }
 
 echo mais($data, $dias);
