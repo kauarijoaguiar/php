bf7c28aaacd3b47e531279c7793c99999217a4d6
juliapcp <juliapcp2003@gmail.com>
2021-10-09 19:45:52 -0300

    uhu

--- indexB.php
+++ indexB.php
@@ -11,47 +11,55 @@ echo "<hr>";
 function mais($data, $dias){
     $data = $_POST['data'];
     $dias = $_POST['dias'];
-    $data = implode('-', array_reverse(explode('/', $_POST['data'])));
-     $ano = $data[0].$data[1].$data[2].$data[3];
-     $days= $data[5].$data[6];
-     $mes= $data[8].$data[9];
-      $data =  mktime(0, 0, 0, $mes, $days, $ano) ."<br>";
-     $pascoa = easter_date($ano); 
-     $diaPascoa = date('j', $pascoa);
-     $mesPacoa = date('n', $pascoa);
-     $contador = 0 ;
-     $feriados = [
+
+    $data = mktime(date("m"),date("d"),date("Y"),
+    date($data[3].$data[4]),date($data[0].$data[1]),date($data[6].$data[7].$data[8].$data[9])). "<br>";
+
+    $dia = date('d', $data);
+    $mes = date('m', $data);
+    $ano = date('Y', $data);
+
+    $data = mktime(0, 0, 0, $mes, $dia, $ano);
+
+    $finalSemana = 0;
+    $diasUteis = 0;
+    $somaDias = 1;
+
+    $pascoa= easter_date($ano);
+    $diaPascoa = date('j', $pascoa);
+    $mesPascoa = date('m', $pascoa);
+
+    $feriados = array(
         mktime(0, 0, 0, 1, 1, $ano), 
         mktime(0, 0, 0, 4, 21, $ano),
         mktime(0, 0, 0, 5, 1, $ano), 
         mktime(0, 0, 0, 9, 7, $ano), 
-        mktime(0, 0, 0, 10,  12, $ano), 
+        mktime(0, 0, 0, 10,  12, $ano),
         mktime(0, 0, 0, 11,  2, $ano), 
         mktime(0, 0, 0, 11, 15, $ano), 
         mktime(0, 0, 0, 12, 25, $ano),
-        mktime(0, 0, 0, $mesPacoa, $diaPascoa, $ano),
-        mktime(0, 0, 0, $mesPacoa, $diaPascoa - 47, $ano),
-        mktime(0, 0, 0, $mesPacoa, $diaPascoa - 2, $ano),
-        mktime(0, 0, 0, $mesPacoa, $diaPascoa + 60, $ano),
-     ];
+        mktime(0, 0, 0, $mesPascoa, $diaPascoa, $ano),
+        mktime(0, 0, 0, $mesPascoa, $diaPascoa - 47, $ano),
+        mktime(0, 0, 0, $mesPascoa, $diaPascoa - 2, $ano),
+        mktime(0, 0, 0, $mesPascoa, $diaPascoa + 60, $ano),
+    );
+    while($diasUteis != $dias){
+        $data = mktime(0, 0, 0, $mes, $dia + $somaDias, $ano)."<br>";
+        if ((date('D', $data) == 'Sat') || (date('D', $data) == 'Sun') ||  in_array($data, $feriados)){
+            $finalSemana++. "<br>";
+        } else {
+            $diasUteis++. "<br>";
+        };
+        $somaDias++. "<br>";
 
-     $contador2 = 1;
-    while($contador != $dias){
-    if(in_array($data, $feriados) || date("w",$data == 0) || date("w",$data == 6)){
-         $soma = $contador2;
-         $contador2++;
-         $contador++;
-    }else{
-        $contador++;
     }
-    }
-    $soma += $dias;
+    
 
-$somadeDU = date('d/m/Y', strtotime("+{$soma} days", strtotime($data)));
-return $somadeDU;
+    return date("d/m/Y",$data)."<br>";
+
 }
-
- echo mais($data, $dias);
+$somadeDU = date('d/m/Y', strtotime("+{$soma} days", strtotime($data)));
+echo mais($data, $dias);
 
 ?>
 </body>
