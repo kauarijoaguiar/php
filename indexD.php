cdcea9d6c316580c69573638754efbaf7f4214f4
juliapcp <juliapcp2003@gmail.com>
2021-10-09 22:52:19 -0300

    -

--- indexD.php
+++ indexD.php
@@ -8,8 +8,39 @@ var_dump($_POST);
 echo "</pre>";
 echo "<hr>";
 
+function calcular($primeiroNumero, $operador, $segundoNumero){
+    $primeiroNumero = (int) $primeiroNumero;
+    $segundoNumero = (int)  $segundoNumero;
+    switch($operador){  
+        case '+':
+        return $primeiroNumero+ $segundoNumero;
 
+        break;
 
+        case '-':
+        return $primeiroNumero- $segundoNumero;
+        break;
+
+        case '*':
+        return $primeiroNumero* $segundoNumero;
+        break;
+
+        case '/':
+        return $primeiroNumero/ $segundoNumero;
+        break;
+
+        default:
+        return "";
+    }   
+}
+
+function converteCardinalParaNumeral($cardinal){
+    return $numeral;
+}
+
+echo calcular(converteCardinalParaNumeral($_POST['primeiroNumero']), 
+    $_POST['operador'], 
+    converteCardinalParaNumeral($_POST['segundoNumero']));
 
 
 ?>
