67e9ab2aedcd5f922f44d0f08b63799d73054909
juliapcp <juliapcp2003@gmail.com>
2021-10-09 23:05:41 -0300

    -

--- indexE.php
+++ indexE.php
@@ -8,33 +8,34 @@ var_dump($_POST);
 echo "</pre>";
 echo "<hr>";
 
-$romans = array(
-    'M' => 1000,
-    'CM' => 900,
-    'D' => 500,
-    'CD' => 400,
-    'C' => 100,
-    'XC' => 90,
-    'L' => 50,
-    'XL' => 40,
-    'X' => 10,
-    'IX' => 9,
-    'V' => 5,
-    'IV' => 4,
-    'I' => 1,
-);
+function romanoParaArabico($romano){
+    $romanos = array(
+        'M' => 1000,
+        'CM' => 900,
+        'D' => 500,
+        'CD' => 400,
+        'C' => 100,
+        'XC' => 90,
+        'L' => 50,
+        'XL' => 40,
+        'X' => 10,
+        'IX' => 9,
+        'V' => 5,
+        'IV' => 4,
+        'I' => 1,
+    );
 
-$roman = $_POST['operador'];
-$result = 0;
+    $resultado = 0;
 
-foreach ($romans as $key => $value) {
-    while (strpos($roman, $key) === 0) {
-        $result += $value;
-        $roman = substr($roman, strlen($key));
+    foreach ($romanos as $chave => $valor) {
+        while (strpos($romano, $chave) === 0) {
+            $resultado += $valor;
+            $romano = substr($roman, strlen($chave));
+        }
     }
-}
-echo $result;
+    return  $resultado;
 
+}
 
 
 ?>
